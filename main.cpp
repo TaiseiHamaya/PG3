18adfaafe7cad1ed71e4a1a557d4d8bef18c897b
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-09-30 12:08:47 +0900

    01_02

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
-﻿#include <iostream>
+#include <iostream>
+
+const uint32_t Wages(uint32_t hour) {
+	if (hour == 1) {
+		return 100;
+	}
+	else {
+		return Wages(hour - 1) * 2 - 50;
+	}
+}
 
 int main() {
-
+	uint32_t hour = 0;
+	constexpr uint32_t WagesParHour = 1072;
+	uint32_t wagesNormal = 0;
+	uint32_t wagesRecursive = 0;
+	do {
+		++hour;
+		wagesNormal = WagesParHour * hour;
+		wagesRecursive = Wages(hour);
+	} while (wagesNormal > wagesRecursive);
+	
+	std::cout << "Hour : " << hour << std::endl;
+	std::cout << "Normal : " << wagesNormal << std::endl;
+	std::cout << "Recursive : " << wagesRecursive << std::endl;
+	return 0;
 }
