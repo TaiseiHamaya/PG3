465189bb95fb67ee10a251c6b1b0cf5e0f24da59
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-09-30 14:20:11 +0900

    01_04

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
-﻿#include <iostream>
+#include <iostream>
+#include <random>
+#include <thread>
+#include <functional>
+#include <string>
+
+void PrintResult(bool result) {
+	std::function<std::string(bool&)> ynLambda = [](bool result)->std::string { return result ? "正解" : "不正解"; };
+
+	std::cout << ynLambda(result) << std::endl;
+}
+
+void CallBack(uint32_t ms, int input, int randomValue) {
+	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+	int mod2 = randomValue % 2;
+	std::cout << (mod2 ? "半" : "丁") << " " << randomValue << std::endl;
+	PrintResult(input == mod2);
+}
 
 int main() {
+	std::random_device device;
+	std::mt19937 mt{ device() };
+	std::uniform_int_distribution random01{ 1,6 };
 
+	int value = random01(mt);
+	int input;
+
+	std::cout << "丁半博打" << std::endl;
+	std::cout << "丁 : 0" << std::endl;
+	std::cout << "半 : 1" << std::endl;
+
+	std::cin >> input;
+
+	std::cout << "正解は" << std::endl;
+	CallBack(3000, input, value);
 }
