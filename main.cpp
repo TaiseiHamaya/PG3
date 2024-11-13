59bbdf9c123633f9577c5886340ec7dce1d81b7d
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-11-13 10:31:55 +0900

    PG3_02_02

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
-﻿#include <iostream>
+#include <iostream>
+
+template<typename T, typename U>
+class TestTemplate {
+public:
+	T Min(T t, U u) {
+		if (t < u) {
+			return t;
+		}
+		else {
+			return static_cast<T>(u);
+		}
+	}
+};
 
 int main() {
+	TestTemplate<int, int> intint;
+	TestTemplate<int, float> intfloat;
+	TestTemplate<int, double> intdouble;
+	TestTemplate<float, float> floatfloat;
+	TestTemplate<float, double> doublefloat;
+	TestTemplate<double, double> doubledouble;
 
+	std::cout << "T = int, U = int : " << intint.Min(1, 2) << std::endl;
+	std::cout << "T = int, U = float : " << intfloat.Min(1, 2) << std::endl;
+	std::cout << "T = int, U = double : " << intdouble.Min(1, 2) << std::endl;
+	std::cout << "T = float, U = float : " << floatfloat.Min(1, 2) << std::endl;
+	std::cout << "T = float, U = double : " << doublefloat.Min(1, 2) << std::endl;
+	std::cout << "T = double, U = double : " << doubledouble.Min(1, 2) << std::endl;
 }
