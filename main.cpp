317918aa09044f7181c142fcb07e90a41d0ca17e
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-11-13 10:42:10 +0900

    PG3_02_03

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
-﻿#include <iostream>
+#include <iostream>
+
+#include <memory>
+#include <list>
+
+class Animal {
+public:
+	Animal() = default;
+	virtual ~Animal() = default;
+
+public:
+	virtual void cry() {};
+};
+
+class Cat : public Animal {
+public:
+	void cry() {
+		std::cout << "にゃー" << std::endl;
+	};
+};
+
+class Dog: public Animal {
+public:
+	void cry() {
+		std::cout << "ワン" << std::endl;
+	};
+};
 
 int main() {
+	std::list<std::unique_ptr<Animal>> animals;
+	animals.emplace_back(std::make_unique<Cat>());
+	animals.emplace_back(std::make_unique<Dog>());
 
+	for (auto& animal : animals) {
+		animal->cry();
+	}
 }
