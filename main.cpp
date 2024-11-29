16be2024db306d28a63776089ddcf3c5df634e9a
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-11-30 01:27:10 +0900

    03_01

--- main.cpp
+++ main.cpp
@@ -1,5 +1,64 @@
-﻿#include <iostream>
+#include <iostream>
+
+#include <list>
 
 int main() {
+	// list<const char*>とかキモすぎる…
+	// あとこの記法許されている理由が静的変数の仕様であることを初めて知った。
+	std::list<const char*> yamanoteLine = {
+	"Tokyo",
+	"Kanda",
+	"Akihabara",
+	"Okachimachi",
+	"Ueno",
+	"Uguisudani",
+	"Nippori",
+	//"Nishi-Nippori",
+	"Tabata",
+	"Komagome",
+	"Sugamo",
+	"Otsuka",
+	"Ikebukuro",
+	"Mejiro",
+	"Takadanobaba",
+	"Shin-Okubo",
+	"Shinjuku",
+	"Yoyogi",
+	"Harajuku",
+	"Shibuya",
+	"Ebisu",
+	"Meguro",
+	"Gotanda",
+	"Osaki",
+	"Shinagawa",
+	//"Takanawa Gateway",
+	"Tamachi",
+	"Hamamatsucho",
+	"Shimbashi",
+	"Yurakucho"
+	};
 
+	std::cout << "1970" << std::endl;
+	for (const auto& station : yamanoteLine) {
+		std::cout << station << std::endl;
+	}
+	std::cout << std::endl;
+
+	// 追加
+	yamanoteLine.emplace(std::next(yamanoteLine.begin(), 7), "Nishi-Nippori");
+
+	std::cout << "2019" << std::endl;
+	for (const auto& station : yamanoteLine) {
+		std::cout << station << std::endl;
+	}
+	std::cout << std::endl;
+
+	// 追加
+	yamanoteLine.emplace(std::next(yamanoteLine.begin(), 25), "Takanawa Gateway");
+
+	std::cout << "2022" << std::endl;
+	for (const auto& station : yamanoteLine) {
+		std::cout << station << std::endl;
+	}
+	std::cout << std::endl;
 }
