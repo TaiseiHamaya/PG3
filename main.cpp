2a10ec56b1fde2b8b6402000d60d517539f05744
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-12-20 15:15:49 +0900

    06_02

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
-﻿#include <iostream>
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+void show() {
+	static int  num = 1;
+	static std::mutex mutex;
+	std::lock_guard lock{ mutex };
+	std::cout << "thread " << num << std::endl;
+	++num;
+ }
 
 int main() {
-
+	std::thread th1{ show };
+	std::thread th2{ show };
+	std::thread th3{ show };
+	if (th1.joinable()) {
+		th1.join();
+	}
+	if (th2.joinable()) {
+		th2.join();
+	}
+	if (th3.joinable()) {
+		th3.join();
+	}
 }
