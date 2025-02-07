3e339f0f75e36f79ba60e611db55696fda21b6d5
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2025-02-07 22:21:21 +0900

    07_01

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
-﻿#include <iostream>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <format>
 
 int main() {
+	std::string copyFrom(100000, 'A');
+	std::string copyTo;
+	std::string moveFrom(100000, 'A');
+	std::string moveTo;
 
+	using timepoint = std::chrono::steady_clock::time_point;
+	using duration = std::chrono::steady_clock::duration;
+
+	timepoint startCopy = std::chrono::high_resolution_clock::now();
+	copyTo = copyFrom;
+	timepoint endCopy = std::chrono::high_resolution_clock::now();
+
+	timepoint startMove = std::chrono::high_resolution_clock::now();
+	moveTo = std::move(moveFrom);
+	timepoint endMove = std::chrono::high_resolution_clock::now();
+
+	std::cout << std::format("Copy : {}\n", endCopy - startCopy);
+	std::cout << std::format("Move : {}\n", endMove - startMove);
 }
